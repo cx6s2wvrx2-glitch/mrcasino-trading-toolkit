28d3f8d094f49dc516eed3e3ddb910e518817a95
cx6s2wvrx2-glitch <cx6s2wvrx2@privaterelay.appleid.com>
2026-09-01 23:33:38 +0300

    Export independent validation agent

--- xauusd-system-v2/src/xauusd_v2/agents/__init__.py
+++ xauusd-system-v2/src/xauusd_v2/agents/__init__.py
@@ -1,4 +1,10 @@
 from .knowledge_agent import KnowledgeAgent
 from .rules_agent import RulesAgent
+from .validation_agent import IndependentValidationAgent, IndependentValidationDecision
 
-__all__ = ["KnowledgeAgent", "RulesAgent"]
+__all__ = [
+    "KnowledgeAgent",
+    "RulesAgent",
+    "IndependentValidationAgent",
+    "IndependentValidationDecision",
+]
