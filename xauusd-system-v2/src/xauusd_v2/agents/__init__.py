ac2b8fa07b05dc5c2231600b05387db7e1f4f19d
cx6s2wvrx2-glitch <cx6s2wvrx2@privaterelay.appleid.com>
2026-09-01 23:35:48 +0300

    Export XAUUSD data agent 04

--- xauusd-system-v2/src/xauusd_v2/agents/__init__.py
+++ xauusd-system-v2/src/xauusd_v2/agents/__init__.py
@@ -1,10 +1,14 @@
 from .knowledge_agent import KnowledgeAgent
 from .rules_agent import RulesAgent
 from .validation_agent import IndependentValidationAgent, IndependentValidationDecision
+from .data_agent import XAUUSDDataAgent, MarketBar, MarketDataValidationReport
 
 __all__ = [
     "KnowledgeAgent",
     "RulesAgent",
     "IndependentValidationAgent",
     "IndependentValidationDecision",
+    "XAUUSDDataAgent",
+    "MarketBar",
+    "MarketDataValidationReport",
 ]
